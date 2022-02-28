20d2235383763515fcd73c8b72b81025bca5c006
spykelion <ndilionel6@gmail.com>
2022-02-28 12:39:22 +0100

    rendered some todo items 🤗

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,25 @@
 <script setup>
+import { store } from "../store";
+console.log(store.todos)
 </script>
 
 <template>
-    <div class="todo">
-        <p>Welcome to todo nation</p>
-        <div class="content">Here is a list of all todo items</div>
-    </div>
+  <div class="todo">
+    <p>Welcome to todo nation</p>
+    <div class="content">Here is a list of all todo items</div>
+  </div>
+  <ul>
+    <li v-for="(todo) in store.todos" :key="todo.id">
+      {{ todo.body }}
+    </li>
+  </ul>
 </template>
 
 <style lang="scss">
-    .todo {
-        .content {
-            color: blue;
-        }
-    }
+.todo {
+  .content {
+    color: blue;
+  }
+}
 </style>
+
--- src/store.js
+++ src/store.js
@@ -1,22 +1,32 @@
 import { reactive } from "vue";
 const today = new Date();
-const dateCreated = today.getDate()  + "-" + (today.getMonth()+1) + "-" + d.getFullYear() + " " +
-today.getHours() + ":" + today.getMinutes() + today.getSeconds();
-const dateUpdated = today.getDate()  + "-" + (today.getMonth()+1) + "-" + d.getFullYear() + " " +
-today.getHours() + ":" + today.getMinutes() + today.getSeconds();
+const date = today.getDate()  + "-" + (today.getMonth()+1) + "-" + today.getFullYear() + " " +
+today.getHours() + ":" + today.getMinutes() + ":" +today.getSeconds();
+const dateCreated = date
+const dateUpdated = date
 
-const initialTodo = {
+let initialTodo = {
     id: 0,
     title: "Created Level 3 design",
-    body: "I minimized the interface to a much smaller level",
+    body: "I minimized the snub component to a much smaller level",
+    completed: false,
     dateCreated,
     dateUpdated
 
 }
 export const store = reactive({
-    todo: [initialTodo*3],
-    create(){
-        
+    todos: [initialTodo,initialTodo,initialTodo],
+    create(obj){
+        const newTodo = obj
+        this.todos = [...this.todos, newTodo]
+    },
+    todoss(){ return this.todos },
+    delete(todo){
+       this.todo =  this.todos.filter(t=>t!=todo)
+    },
+    update(todo){
+        let todoObject =  this.todos.filter(t=>t==todo)
+        todoObject = todo
     },
 
 })
