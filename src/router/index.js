f294ac76d77f1738c7481d60aa914690baa45bb2
spykelion <ndilionel6@gmail.com>
2022-03-01 13:52:10 +0100

    setup router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: ''
+    },
+    {
+        path: '/todo/:id',
+        name: 'Detail',
+        component: ''
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL), routes
+})
+
+export default router
